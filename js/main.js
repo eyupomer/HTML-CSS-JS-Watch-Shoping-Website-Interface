349b2235511545c55c1d1b9cb201438c9e0ece05
eyupomer <odabasieyupomer@gmail.com>
2022-05-03 21:35:36 +0300

    footer completed, starting responsive design

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+// Resize Navbar on Scroll
+
+var navbar = document.querySelector('.navbar');   // when the scroll is higher than 20 viewport height, add the sticky class to the tag with a class navbar
+window.onscroll = () => {
+    this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+}
+
 // store product info in object
 
 var pic = document.querySelector('#main-shoe');
